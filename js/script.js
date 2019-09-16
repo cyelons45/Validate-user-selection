5925cc218d852aef8cf09978efe7e530caec802f
samuel Nyarkoh <cyelons45@yahoo.com>
2019-09-15 21:05:26 -0400

    changed footer font size

--- js/script.js
+++ js/script.js
@@ -6,16 +6,18 @@ document.querySelector(".bikes").addEventListener("click", function(e) {
     let selection = selected.parentNode;
     let name = selection.querySelector(".bike__name").innerHTML;
     let val = selection.querySelector(".bike__price-value").innerHTML;
+    let type = selection.querySelector(".bike__type-item").innerHTML;
     let image = selection.querySelector(".bike__img")["src"];
     selected_obj = {
-      selection: name,
-      selection_type: val,
+      selection_name: name,
+      selection_val: val,
+      selection_type: type,
       selection_image: image
     };
     // console.log(selected_obj);
     document.querySelector(
       ".popup__yourselection"
-    ).innerHTML = `Thank you for selecting ${selected_obj.selection}!`;
+    ).innerHTML = `Thank you for selecting ${selected_obj.selection_name}!`;
     replaceItem(selected_obj);
   }
 });
@@ -28,15 +30,15 @@ function replaceItem(selected_obj) {
   <svg class="bike__like">
     <use xlink:href="img/sprite.svg#icon-heart-full"></use>
   </svg>
-  <h5 class="bike__name">${selected_obj.selection}</h5>
+  <h5 class="bike__name">${selected_obj.selection_name}</h5>
   <div class="bike__type">
     <div class="bike__type-name">Type :</div>
-    <p class="bike__type-item">Bike</p>
+    <p class="bike__type-item">${selected_obj.selection_type}</p>
   </div>
 
   <div class="bike__price">
     <div class="bike__price-name">Price:</div>
-    <p class="bike__price-value">${selected_obj.selection_type}</p>
+    <p class="bike__price-value">${selected_obj.selection_val}</p>
   </div>
 </div>
 
